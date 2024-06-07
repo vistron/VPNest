cdb7dc0d56fc90c5526a5aaa18f5f375008af4a1
vistron <vishnuEIN@gmail.com>
2024-06-07 13:17:59 +0530

    [DEV]: Added basic Geometry

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "VP Nest",
-  description: "Housewarming for VPNest"
+  description: "Housewarming Ceremony"
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,54 @@
+'use client';
+
 import Image from "next/image";
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const loader = new GLTFLoader();
-
+import React, { useRef, useEffect } from 'react';
 
 
 
 export default function Home() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+      renderer.render(scene, camera);
+      containerRef.current?.appendChild(renderer.domElement);
+      camera.position.z = 5;
+
+      const renderScene = () => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+        requestAnimationFrame(renderScene);
+      };
+      const handleResize = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(width, height);
+      };
+      window.addEventListener('resize', handleResize);
+      renderScene();
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div ref={containerRef} />
     </main >
   );
 }
